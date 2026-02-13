b2671e966c832e28c1c6717773b354d61a3456f5
ar510 <ar510@gmail.com>
2026-02-13 19:08:29 +0330

    Testing Application
    Check for Bug in Debug

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 import 'package:provider/provider.dart';
 import 'providers/program_provider.dart';
 import 'screens/main_screens/home_screen.dart';
